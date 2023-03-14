eb88f884e124e2a995b6b657d6b741b79f6829ad
Harsh Patel <hpatel1959@hotmail.com>
2023-03-14 23:12:28 +0000

    implement basic login structure

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,16 +1,3 @@
 $(() => {
 
-  $('#login-form').on('submit', function(event) {
-    event.preventDefault();
-
-    const $formData = $(this).serialize();
-
-    $.ajax({
-      method: 'POST',
-      url: '/login',
-      data: $formData
-    }).catch((err) => {
-      res.status(500).send(err);
-    })
-  })
 })
--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router  = express.Router();
+const db = require('../db/connection');
 
 router.get('/', (req, res) => {
   res.render('login');
 });
 
 router.post('/', (req, res) => {
-  console.log(req.body);
+  const { email, password } = req.body;
+
+  const queryString = 'SELECT * FROM users WHERE email = $1';
+
+  db.query(queryString, [email])
+  .then((data) => {
+    if (!data.rows[0]) {
+      res.send('Invalid login');
+    }
+
+    res.redirect('/');
+  })
+  .catch(err => {
+    console.log('Error: ', err);
+  });
+
+
 })
 module.exports = router;
