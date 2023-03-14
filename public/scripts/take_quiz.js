4d8b29b06497ed0e27d8ddee2acc08f99e7c5e91
Harsh Patel <hpatel1959@hotmail.com>
2023-03-14 18:43:45 +0000

    attempt to fix bug where api PK is different from database PK

--- public/scripts/take_quiz.js
+++ public/scripts/take_quiz.js
@@ -1,3 +1,7 @@
+
+
+
+
 $(() => {
   const pathname = window.location.pathname.split('/')
   const quiz_id = pathname[pathname.length - 1]
@@ -7,6 +11,6 @@ $(() => {
     url: `http://localhost:8080/api/questions/${quiz_id}/questions`
   })
   .then(data => {
-    console.log(data);
+    console.log(data.questions);
   })
 })
