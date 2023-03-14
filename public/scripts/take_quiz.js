4378ee0907aa5b17f27933b525d4bff72c8e43ec
Harsh Patel <hpatel1959@hotmail.com>
2023-03-14 20:05:37 +0000

    start createQuestion element function in take_quiz.js

--- public/scripts/take_quiz.js
+++ public/scripts/take_quiz.js
@@ -1,4 +1,55 @@
+const createQuestionElement = function(questionObj) {
+  // variables from user object to be used in object
+  const name = questionObj.name;
+  const category = questionObj.category;
+  const description = questionObj.description;
+  const numOfQuestions = questionObj.num_of_question;
+  const question = questionObj.question;
+  const option1 = questionObj.option_1;
+  const option2 = questionObj.option_2;
+  const option3 = questionObj.option_3;
+  const option4 = questionObj.option_4;
 
+
+  // new quiz article
+  const newArticle = $("<article class='quizDisplay'>");
+
+  // quiz header elements
+  const newHeader = $("<header class='quizHeader'>");
+  const newHeaderDiv = $("<div>");
+  const newQuizCategory = $(`<span class='quizCategory'>`).text(`${category}`);
+  const newQuizName = $("<p class='quizName'>").text(`${name}`);
+
+  newHeaderDiv.append(newQuizName);
+  newHeaderDiv.append(newQuizCategory);
+  newHeader.append(newHeaderDiv);
+
+  // quiz body elements
+  // added quiz description for css
+  const newQuizDescriptionP = $("<p class='quizDescription'>").text(`${description}`);
+
+  // quiz footer elements
+  const newFooter = $("<footer class='QuizFooter'>");
+  // const newFooterP = $("<p>").text(`${quizDate}`);
+  const newFooterDiv = $("<div>");
+  // changed it to dynamic
+  const newQuizNumOfQuestions = $("<p class='quizNumOfQuestions'>").text( `Total Questions: ${numOfQuestions}`);
+  const newQuizCreatorName = $("<p class='quizCreatorName'>").text(`Created By: Dave`);
+  const newShareBtnDiv = $("<div><i class='bi bi-share-fill'></i></div>")
+
+  newFooterDiv.append(newQuizCreatorName);
+  newFooterDiv.append(newQuizNumOfQuestions);
+  newFooter.append(newFooterDiv);
+  newFooterDiv.append(newShareBtnDiv);
+
+
+  // adding new elements to article
+  newArticle.append(newHeader);
+  newArticle.append(newQuizDescriptionP);
+  newArticle.append(newFooter);
+
+  return newArticle;
+};
 
 
 
