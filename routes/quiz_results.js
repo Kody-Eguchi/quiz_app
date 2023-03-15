bdedb658f447636844a97216001028251b780b49
Koji Eguchi <koji.eguchi@hotmail.com>
2023-03-14 20:48:22 -0400

    Add POST request to send answered questions data to quiz_results

--- routes/quiz_results.js
+++ routes/quiz_results.js
@@ -7,7 +7,20 @@ router.get('/', (req, res) => {
   res.render('quiz_results');
 });
 
+// URL should be wildcard /:id
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 
+//   const queryStringOne = `
+//     INSERT INTO answered_questions (question_id, given_answer)
+//     VALUES ($1, $2);
+//   `;
+// db.query()
+// .then()
+// .catch();
+
+})
 
 
 module.exports = router;
