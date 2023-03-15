66a87efbf6e921bbc54147d408eb8a0075528792
Koji Eguchi <koji.eguchi@hotmail.com>
2023-03-15 19:04:52 -0400

    Set API routes with specific quiz results

--- routes/show_quiz_results.js
+++ routes/show_quiz_results.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/:quiz_result_id', (req, res) => {
+
   res.render('show_quiz_results');
 });
 
-router.get('/api/quiz_results', (req, res) => {
+router.get('/:quiz_result_id/json', (req, res) => {
+  console.log(req.body);
   res.json(req.body);
 })
 
