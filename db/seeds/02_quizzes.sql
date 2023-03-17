1efb2026db7d08f0b26618cb5caa9a9991bf3cf4
Harsh Patel <hpatel1959@hotmail.com>
2023-03-17 04:10:41 +0000

    change seed data for more realistic examples

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,6 +1,6 @@
 INSERT INTO quizzes (creator_id, description, category, name, num_of_question, is_public)
 VALUES (2, 'description', 'Music', 'my quiz', 2, FALSE),
-(3, 'description', 'Math', 'Mathletes', 5, TRUE),
-(5, 'description', 'Science', 'Einstein', 3, TRUE),
-(4, 'description', 'History', 'Dinosaurs', 4, FALSE),
-(1, 'description', 'Arts', 'Quiz for you', 1, TRUE);
+(3, 'Test your math skills!', 'Math', 'Mathletes', 5, TRUE),
+(5, 'Test your physics skills!', 'Science', 'E=MC Hammer', 3, TRUE),
+(4, 'Test your knowledge about dinosaurs!', 'History', 'Throwback!', 4, TRUE),
+(1, 'Test your knowledge on famous paintings!', 'Arts', 'The Quizzy Night', 1, TRUE);
